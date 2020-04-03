7a145913a081b0520fca062205640c0b0069c3d8
Wleydson <wleydsonredes@gmail.com>
2020-04-03 18:53:52 -0300

    todo-list project completed without redux

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import PageHeader from '../template/pageHeader'
 
-export default props =>(
+export default props => (
     <div>
-        <PageHeader name="Sobre" small="Nós" />
-        
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
         <h2>Nossa História</h2>
         <p>Lorem ipsum dolor sit amet...</p>
         <h2>Missão e Visão</h2>
--- frontend/src/template/grid.jsx
+++ frontend/src/template/grid.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-export default class Grid extends Component{
-    toCssClasses(numbers){
+export default class Grid extends Component {
+    toCssClasses(numbers) {
         const cols = numbers ? numbers.split(' ') : []
         let classes = ''
 
